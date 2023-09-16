24ec297166b3c44b35ba39e158551954e4c623d2
patrickssj222 <patrickssj222@gmail.com>
2023-09-16 17:44:25 -0400

    finished homepage

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-/*
 import stockVideo from 'asset/video/stockVideo.mp4';
-*/
+import './Home.scss';
 
 export function Home() {
   return (
-    <div>
-      <p>Hi</p>
-      {/* <video className="videoTag" autoPlay loop muted>
+    <div className="container">
+      <div className="navbar">
+        <p className="navbarText">Tesla Body Shop</p>
+      </div>
+      <div className="body">
+        <p />
+        <h1 className="title">Just a title</h1>
+        <button className="button" type="button">Start Building</button>
+      </div>
+      <video className="video" autoPlay loop muted>
         <source src={stockVideo} type="video/mp4" />
-      </video> */}
+      </video>
     </div>
   );
 }
