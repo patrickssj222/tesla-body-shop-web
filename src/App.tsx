ba8d6ac2ab2cdbc19d859d7a77ee2de2a25abdbc
patrickssj222 <patrickssj222@gmail.com>
2023-09-16 19:42:12 -0400

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Home } from 'pages/Home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { ModelSelect } from 'pages/ModelSelect/ModelSelect';
+
 import { ModelEdit } from 'pages/ModelEdit/ModelEdit';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="model-select" element={<ModelSelect />} />
         <Route path="model-edit" element={<ModelEdit />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
