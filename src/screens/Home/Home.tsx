2327715a5b2af2b3888f2e320f17feca13983a7d
patrickssj222 <patrickssj222@gmail.com>
2023-09-16 16:45:17 -0400

    finished dealing wiht eslint

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+/*
+import stockVideo from 'asset/video/stockVideo.mp4';
+*/
+
+export function Home() {
+  return (
+    <div>
+      <p>Hi</p>
+      {/* <video className="videoTag" autoPlay loop muted>
+        <source src={stockVideo} type="video/mp4" />
+      </video> */}
+    </div>
+  );
+}
